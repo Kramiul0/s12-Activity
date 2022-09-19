bffc3ecf1bf929db4d7a2c6b1d52f2691c7c05d4
JohnChristopher <jjavier@vertere-gs.com>
2022-09-19 14:59:10 +0800

    Add activity code

---
+++ index.js
@@ -0,0 +1,53 @@
+//Objective 1
+
+let firstName = 'First Name: John';
+console.log(firstName);
+
+let lastName = 'Last Name: Javier';
+console.log(lastName);
+
+let age = 24;
+console.log('Age: '+ age);
+
+let want = 'Hobbies: ';
+console.log(want);
+let hobbies = ['badminton', 'volleyball' , 'computer games'];
+console.log(hobbies);
+
+let desc = 'Work Address:';
+console.log(desc);
+let work = {
+	houseNumber:447,
+	street: 'Pasolo Rd',
+	city: 'Valenzuela City',
+	state: 'Philippines'
+};
+console.log(work);
+
+
+//Objective 2
+let fullName = 'Steve Rogers';
+console.log('My full name is: '+ fullName);
+
+let ages = 40;
+console.log('My current age is: '+ ages);
+
+let descFriend = 'My Friends are: ';
+console.log(descFriend);
+let friend = ['Tony', 'Bruce', 'Thor', 'Natasha', 'Clint', 'Nick'];
+console.log(friend);
+
+let descProfile = 'My Full Profile: ';
+console.log(descProfile);
+let fullProfile = {
+	username: 'captain_america',
+	fullName: 'Steve Rogers',
+	age: 40,
+	isActive: false
+};
+console.log(fullProfile);
+
+let bestFriend = 'Bucky Barnes';
+console.log('My bestfriend is: '+ bestFriend);
+let place = 'Artic Ocean';
+console.log('I was found frozen in: '+ place);
